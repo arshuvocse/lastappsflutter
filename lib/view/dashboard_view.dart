40ff7ba0c2deb1411950a31a5a3020609f21e0f2
shuvo.creatrixbd <shuvo.creatrixbd@gmail.com>
2025-11-05 08:43:15 +0000

    fix: Remove unused import

--- lib/view/dashboard_view.dart
+++ lib/view/dashboard_view.dart
@@ -1,6 +1,5 @@
 
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:myapp/view/approval_view.dart';
 import 'package:myapp/view/settings_view.dart';
 
